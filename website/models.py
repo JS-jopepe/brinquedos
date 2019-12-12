bd3de6305a912558f1e7a46657051eed841847d2
JS-jopepe <jovenel.s@outlook.com>
2019-12-11 22:59:51 -0300

    Mexendo no views

--- website/models.py
+++ website/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class pedido(models.Model):
+    nome = models.CharField(max_length=20)
+    
--- website/views.py
+++ website/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    nome = 'Jovenel'
+    idade = '16'
+    text = 'Vem conhecer os nossos brinquedos'
+    lista_brinquedos = [
+        'Banco Imobilário',
+        'Lol Surprise',
+        'Boneca Reborn',
+        'Shopkins',
+        'Angie Oral Care',
+        'Hatchimals',
+        'John Deere Construction',
+        'Baby Madeira ',
+    ]
+
+    context = {
+        'brinquedos' :  lista_brinquedos,
+        'nome' : nome
+        'idade' : idade
+        'text' : text
+    }
+
+    return render(request, 'home.html', context)
